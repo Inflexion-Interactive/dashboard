c72bf4d45c317549c364e09ff0b83a38b7b12f37
Kevin Pruett <pruett.kevin@gmail.com>
2015-03-19 17:58:12 -0400

    progress

--- test/index.js
+++ test/index.js
@@ -3,15 +3,40 @@ var request = require('supertest'),
     app = require('./../lib/index');
 
 describe('Request to root', function() {
+
   it('Returns 200 status code', function(done) {
 
     request(app)
       .get('/')
-      .expect(200)
-      .end(function(error) {
-        if (error) throw error;
-        done();
-      });
+      .expect(200, done)
+
+  });
+});
+
+describe('Request to Google Analytics', function() {
+
+  it('Returns 200', function(done) {
+
+    request(app)
+      .get('/google')
+      .expect(200, done)
+
+  });
+
+  it('Returns json content', function(done) {
+
+    request(app)
+      .get('/google')
+      .expect('Content-Type', /json/, done);
+
+  });
+
+  it('Returns a payload', function(done) {
+
+    request(app)
+      .get('/google')
+      .expect(JSON.stringify({ status: 'ok' }), done);
+
   });
 });
 
